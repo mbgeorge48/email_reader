5b0ac0a0e82ab9e7680a1a1e3095da98c2c8b37a
Matt George <mbgeorge48@gmail.com>
2023-01-22 22:16:29 +0000

    Only mark the emails as read if theyre older than 7 days

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import imaplib
+import datetime
 import email
+import imaplib
 import json
+
 from config_reader import ConfigReader
 
 config = ConfigReader()
 log = []
+a_week_ago = (datetime.datetime.today() - datetime.timedelta(days=7)).date()
 
 M=imaplib.IMAP4_SSL(config.imap_server,config.imap_port)
 M.login(config.gmail_username,config.gmail_password)
@@ -13,15 +16,14 @@ typ, data = M.search(None, 'UNSEEN')
 for num in data[0].split():
     typ, data = M.fetch(num, '(RFC822)')
     msg = email.message_from_bytes(data[0][1])
-    log.append({'subject': msg["Subject"], 'from':msg['From'], 'date': msg['Date']})
-    M.store(num, '+FLAGS', '\\Seen')
+    if (datetime.datetime.strptime(msg['Date'][:25], '%a, %d %b %Y %H:%M:%S').date()) < a_week_ago:
+        log.append({'subject': msg["Subject"], 'from':msg['From'], 'date': msg['Date']})
+        M.uid("STORE",num, '+X-GM-LABELS', '(autoread)')
+        M.uid("STORE",num, '+FLAGS', '\\Seen')
+    else:
+        M.uid("STORE",num, '-FLAGS', '\\Seen')
 M.close()
 M.logout()
 
 with open('log_file.json', 'a') as f:
     json.dump(log, f, indent=4, ensure_ascii=False)
-
-
-# TODO Add readme with some instructions to setting up
-# TODO Add a date filter so only emails older than x days get marked as read
-# TODO Maybe append the new logs to the top of the log file?
